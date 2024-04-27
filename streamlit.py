2d1d04603560d451cae0154dc9aaf80cdb6ce20e
Ayush Soni <ayush77.2000@gmail.com>
2024-04-27 18:17:04 +0530

    updated gitignore

--- streamlit.py
+++ streamlit.py
@@ -42,3 +42,6 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+'''pyinstaller streamlit.py --onefile --nowindow --add-data .\processing.py:. 
+--add-data .\retinet_9010.h5:. --add-data .\README.md:. --paths'''
